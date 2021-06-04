2a9da3cc31c6682db5b01b2570ed5721298b531f
Mayur Wankhade <mayur.wankhade2@gmail.com>
2021-06-04 13:59:23 +0530

    [Mayur] Add : Added class to create contact.

---
+++ AddressBookSystem/AddressBookSystem/AddressBook.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookSystem
+{
+    class AddressBook
+    {
+        // private  variables declared
+        private string _firstName;
+        private string _lastName;
+        private string _address;
+        private string _city;
+        private string _state;
+        private int _zip;
+        private long _phoneNumber;
+        private string _email;
+
+        // using accessors to get and set the value
+        public string FirstName
+        {
+            get { return this._firstName; }
+            set { this._firstName = value; }
+        }
+        public string LastName
+        {
+            get { return this._lastName; }
+            set { this._lastName = value; }
+        }
+        public string Address
+        {
+            get { return this._address; }
+            set { this._address = value; }
+        }
+        public string City
+        {
+            get { return this._city; }
+            set { this._city = value; }
+        }
+        public string State
+        {
+            get { return this._state; }
+            set { this._state = value; }
+        }
+        public int Zip
+        {
+            get { return this._zip; }
+            set { this._zip = value; }
+        }
+        public long PhoneNumber
+        {
+            get { return this._phoneNumber; }
+            set { this._phoneNumber = value; }
+        }
+        public string Email
+        {
+            get { return this._email; }
+            set { this._email = value; }
+        }
+    }
+}
--- AddressBookSystem/AddressBookSystem/Program.cs
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -6,7 +6,28 @@ namespace AddressBookSystem
     {
         static void Main()
         {
-            Console.WriteLine("Welcome to Address Book Program...");
+            // creating object
+            AddressBook book = new AddressBook();
+
+            // calls set accessor of the properties
+            book.FirstName = "Mayur";
+            book.LastName = "W";
+            book.Address = "Navi Mumbai";
+            book.City = "Vashi";
+            book.State = "Maharashtra";
+            book.Zip = 400703;
+            book.PhoneNumber = 808080;
+            book.Email = "mayur.wankhade2@gmail.com";
+
+            // displaying values
+            Console.WriteLine("First Name : " + book.FirstName);
+            Console.WriteLine("Last Name : " + book.LastName);
+            Console.WriteLine("Address : " + book.Address);
+            Console.WriteLine("City : " + book.City);
+            Console.WriteLine("State : " + book.State);
+            Console.WriteLine("Zip : " + book.Zip);
+            Console.WriteLine("Phone Number : " + book.PhoneNumber);
+            Console.WriteLine("Email : " + book.Email);
         }
     }
 }
